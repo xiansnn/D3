4eb55b06759d295f7fa67ff7d8fa029b2c8db128
chris <xiansnn@hotmail.com>
2019-05-06 19:27:31 +0200

    deplacement des parametres d'init dans module __init__.py

--- modelizer/__init__.py
+++ modelizer/__init__.py
@@ -1,2 +1,17 @@
+# creation modele
+import modelizer.modelGenerator as mg
 
-# definir un graphe
+# data_directory = r"C:\Users\chris\Dropbox\D3\d3\datafiles"
+data_directory = r".\datafiles"
+
+# n_file_name = r"\mini_cab_neighborlink.csv"
+# n_file_name = r"\neighborlink.dat"D3\d
+# n_file_name = r"\A319_neighborlink_gd.csv"
+# n_file_name = r"\A319_neighborlink.dat"
+n_file_name = r"\A321S_neighborlink.dat"
+
+# model = mg.Model('A319')
+model = mg.Model('A321S')
+# model = mg.Model('mini_cab')
+
+maintenance_server_name = 'DSU1'
--- modelizer/__main__.py
+++ modelizer/__main__.py
@@ -3,31 +3,15 @@ Created on 10 sept. 2017
 
 @author: chris
 '''
-# from . import DS
-import modelizer.service_module as srv
-import modelizer.resource_module as res
 import modelizer.DSL_generator as dsl
-import modelizer.modelGenerator as mg
+import modelizer.resource_module as res
+from modelizer import service_module as srv
 
+# import des parametres d'initialisation
+from . import *
 
-#
 
 if __name__ == '__main__':
-    # creation modele
-    # data_directory = r"C:\Users\chris\Dropbox\D3\d3\datafiles"
-    data_directory = r".\datafiles"
-
-#     n_file_name = r"\mini_cab_neighborlink.csv"
-    # n_file_name = r"\neighborlink.dat"D3\d
-    # n_file_name = r"\A319_neighborlink_gd.csv"
-#     n_file_name = r"\A319_neighborlink.dat"
-    n_file_name = r"\A321S_neighborlink.dat"
-
-#     model = mg.Model('A319')
-    model = mg.Model('A321S')
-#     model = mg.Model('mini_cab')
-
-    maintenance_server_name = 'DSU1'
 
     neighbor_link_file_name = n_file_name.split(sep='.')
     file_extension = neighbor_link_file_name[-1]
